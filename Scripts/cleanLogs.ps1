0cd07d1cdac0330e18099f1f6cd3420d4cec97d2
Vlad Varvashenko <vlad19972017@gmail.com>
2018-09-09 14:27:59 +0300

    Add proxy.config into Angular Client for proxying 4200 port on 5000 of service, rewrote deploy.ps1 powershell script some change on configuration of web api cuz i dont now how to rout angular from service via start C# application, this trouble will investigate

--- Scripts/cleanLogs.ps1
+++ Scripts/cleanLogs.ps1
@@ -1,6 +1,10 @@
 $logs = "Logs"
-$scriptPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+
+Write-Host $MyInvocation.MyCommand.Definition
+$scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 foreach($log in Get-ChildItem -Path "$scriptPath/../$logs"){
-    Remove-Item -Path $log.FullName -Force -Recurse
+	$path = $log.FullName
+	Write-Host -Path $path 
+    Remove-Item -Path $path -Force -Recurse
 }
