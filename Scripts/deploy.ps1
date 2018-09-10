01426a6f9a24f168ecfe218ff8d49ea737f04b96
Vlad Varvashenko <vlad19972017@gmail.com>
2018-09-10 21:26:07 +0300

    Resolved: deploy angular and back-end by dotnet run, a good configurations

--- Scripts/deploy.ps1
+++ Scripts/deploy.ps1
@@ -2,28 +2,21 @@ $scriptPath = Split-Path -parent $MyInvocation.MyCommand.Definition
 $Launch = "BackToMe";
 $BackEnd = "$scriptPath\.."
 $ClientApp = "$scriptPath\..\ClientApp"
-$defaultPeriodOfTimeToUpBackEnd = 80
 
-[int]$typeOfDeploy = Read-Host "1-Back-End 2-Client 3-Both of`n"
+[int]$typeOfDeploy = Read-Host "1-Client 2-Back-End and Client (SPA)"
 switch ($typeOfDeploy) {
-     1 {         
-         & dotnet.exe run --project $BackEnd --launch-profile $Launch 
-        }
-     2 { 
+     1 { 
          Set-Location -Path $ClientApp 
-         ng serve --serve-path $ClientApp }
-     3 {
+         ng serve 
+	}
+     2 {
         Write-Host "Path to back-end [dotnet.exe ..args..] -> $BackEnd" 
-        Start-Process -FilePath "dotnet.exe" -ArgumentList "run --project $BackEnd --launch-profile $Launch "  -WorkingDirectory $PSScriptRoot 
-        Write-Host "Back-end is upped" -ForegroundColor Green;
-        
-        Start-Sleep -Seconds $defaultPeriodOfTimeToUpBackEnd
-        
-        Write-Host "Path to client [ng serve] -> $ClientApp"
-        
-        Set-Location -Path $ClientApp #ng dont have a arg that serve angular application by path, only from current folder 
-        ng serve
-        Write-Host "Angular client is upped" -ForegroundColor Green; 
+        & dotnet.exe run --project $BackEnd
+        Write-Host "Back-end is upped" -ForegroundColor Green
+        Write-Host "Path to client [ng serve] -> $ClientApp"        
+        #Set-Location -Path $ClientApp #ng dont have a arg that serve angular application by path, only from current folder 
+        #ng serve
+        #Write-Host "Angular client is upped" -ForegroundColor Green; 
     }
     Default {
         Write-Host "Error mode of deployment" -ForegroundColor Red
